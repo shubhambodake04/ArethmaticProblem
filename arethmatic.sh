29cff2f0e96864aec50207ad1b28ae5cf4b2a3ef
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-08 11:55:50 +0530

    [Shubham] added arethmatic.sh

--- arethmatic.sh
+++ arethmatic.sh
@@ -27,9 +27,26 @@ function fourthExpression(){
 }
 fourthExpression
 function storeIntoArray(){
-	for(( index=0; index<${#resultStore[@]}; index++ ))
+	for(( index=0; index<${resultStore[@]}; index++ ))
 	do
 		resultStoreInArray[index]=${resultStore[$index]}
 	done
 }
 storeIntoArray
+function desendingArraySorting(){
+	for (( index=0; index<${#resultStoreInArray[@]}; index++ ))
+	do
+      		temp=0
+      		for (( innerIndex=0; innerIndex<${#resultStoreInArray[@]}; innerIndex++ ))
+      		do
+         		if [[ ${resultStoreInArray[innerIndex]%.*} -lt ${resultStoreInArray[index]%.*} ]]
+         		then
+            			temp=${resultStoreInArray[index]}
+            			resultStoreInArray[index]=${resultStoreInArray[innerIndex]}
+            			resultStoreInArray[innerIndex]=$temp
+         		fi
+      		done
+   done
+echo "array in desending order => " ${resultStoreInArray[@]}
+}
+desendingArraySorting
