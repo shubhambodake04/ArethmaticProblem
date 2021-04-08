b82043b894ed8bb7df3c482830dcb7755809d639
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-08 10:18:23 +0530

    [Shubham] added arethmatic.sh

--- arethmatic.sh
+++ arethmatic.sh
@@ -3,3 +3,9 @@
 read -p "Enter first number:" firstNumber
 read -p "Enter second number:" secondNumber
 read -p "Enter third number:" thirdNumber
+
+function firstExpression(){
+	firstResult=$(( ($firstNumber+$secondNumber*$thirdNumber) ))
+	echo $firstResult
+}
+firstExpression
