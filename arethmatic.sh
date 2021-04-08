d81453691f38e7543f2b14cfacb7384ebb9d5d17
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-08 11:27:01 +0530

    [Shubham] added arethmatic.sh

--- arethmatic.sh
+++ arethmatic.sh
@@ -1,32 +1,35 @@
 #! /bin/bash
-declare -a resultStore
-read -p "Enter first number:" firstNumber
-read -p "Enter second number:" secondNumber
-read -p "Enter third number:" thirdNumber
 
+declare -A resultStore
+declare -a resultStoreInArray
+
+read -p "Enter first Number: " firstNumber
+read -p "Enter second Number: " secondNumber
+read -p "Enter third Number: " thirdNumber
 function firstExpression(){
-	firstResult=$(( ($firstNumber+$secondNumber*$thirdNumber) ))
-	resultStore[firstResult]=$firstResult
-	echo ${resultStore[firstResult]}
+	firstResult=$((($firstNumber+$secondNumber*$thirdNumber)))
+	resultStore[0]=$firstResult
 }
 firstExpression
 function secondExpression(){
-        secondResult=$(( ($firstNumber*$secondNumber+$thirdNumber) ))
-        resultStore[secondResult]=$secondResult
-	echo ${resultStore[secondResult]}
+	secondResult=$((($firstNumber*$secondNumber+$thirdNumber)))
+	resultStore[1]=$secondResul
 }
-secondExpression
+ secondExpression
 function thirdExpression(){
-        thirdResult=$(( ($thirdNumber+$firstNumber/$secondNumber) ))
-        resultStore[thirdResult]=$thirdResult
-	echo ${resultStore[thirdResult]}
-
+	thirdResult=$((($thirdNumber+$firstNumber/$secondNumber)))
+	resultStore[2]=$thirdResult
 }
 thirdExpression
 function fourthExpression(){
-        fourthResult=$(( ($firstNumber%$secondNumber+$thirdNumber) ))
-        resultStore[fourthResult]=$fourthResult
-	echo ${resultStore[fourthResult]}
-
+	fourthResult=$((($firstNumber%$secondNumber+$thirdNumber)))
+	resultStore[3]=$fourthResult
 }
 fourthExpression
+function storeIntoArray(){
+	for(( index=0; index<${#resultStore[@]}; index++ ))
+	do
+		resultStoreInArray[index]=${resultStore[$index]}
+	done
+}
+storeIntoArray
